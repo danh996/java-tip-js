e285dcafe1c7a6c00f9582a6f660c4021325eb5b
Le Cong Danh <congdanh.le@amaris.com>
2023-12-01 06:21:32 +0700

    Init project and connect to Postgres success

---
+++ src/main/java/danh/com/demo/TipJSApplication.java
@@ -0,0 +1,11 @@
+package danh.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipJSApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TipJSApplication.class, args);
+	}
+}
